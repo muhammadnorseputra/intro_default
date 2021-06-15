1430ab19d6974164a863e28ecc2d1a62e9634a04
muhammadnorseputra <muhammadnorseputra@gmail.com>
2021-06-15 11:00:40 +0700

    init

---
+++ index.php
@@ -0,0 +1,196 @@
+<!DOCTYPE html>
+<html lang="en-US" dir="ltr">
+  <head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <!--
+    Document Title
+    =============================================
+    -->
+    <title>Office | Badan Kepegawaian Pendidikan dan Pelatihan Daerah Kab. Balangan</title>
+    <!--
+    Favicons
+    =============================================
+    -->
+    <link rel="icon" type="image/png" sizes="16x16" href="assets/images/logo.png">
+    <!--<link rel="manifest" href="/manifest.json">-->
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="https://www.bkppd-balangankab.info/intro/assets/images/logo.png">
+    <meta name="og:image" content="https://www.bkppd-balangankab.info/intro/assets/images/logo.png">
+    <meta name="og:description" content="Website Resmi Badan Kepegawaian Pendidikan dan Pelatihan Daerah Kabupaten Balangan">
+    <meta name="og:title" content="Office | Badan Kepegawaian Pendidikan dan Pelatihan Daerah Kab. Balangan">
+    <meta name="theme-color" content="dark">
+    <meta name="author" content="bkppd-balangankab.info">
+    <meta name="keywords" content="website resmi,bkppd,bkd,kepegawaian,kepegawaian daerah,balangan,pemerintah daerah,cpns,pns,pensiun,cuti,berkala">
+    <meta name="description" content="Website Resmi Badan Kepegawaian Pendidikan dan Pelatihan Daerah Kabupaten Balangan">
+    <meta name="release" content="2019">
+    <!--
+    Stylesheets
+    =============================================
+    -->
+    <!-- Default stylesheets-->
+    <link href="assets/lib/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Template specific stylesheets-->
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:400,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Volkhov:400i" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800" rel="stylesheet">
+    <link href="assets/lib/animate.css/animate.css" rel="stylesheet">
+    <link href="assets/lib/components-font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <link href="assets/lib/et-line-font/et-line-font.css" rel="stylesheet">
+    <link href="assets/lib/flexslider/flexslider.css" rel="stylesheet">
+    <link href="assets/lib/magnific-popup/dist/magnific-popup.css" rel="stylesheet">
+    <link href="assets/lib/simple-text-rotator/simpletextrotator.css" rel="stylesheet">
+    <link href="assets/lib/countdown/style.css" rel="stylesheet">
+    <!-- Main stylesheet and color file-->
+    <link href="assets/css/style.css" rel="stylesheet">
+    <link id="color-scheme" href="assets/css/colors/default.css" rel="stylesheet">
+    <style>
+    p#demo {
+  text-align: center;
+  font-size: 60px;
+  margin-top: 0px;
+}
+    .blink {
+      animation: blink-animation 1s steps(5, start) infinite;
+      -webkit-animation: blink-animation 1s steps(5, start) infinite;
+    }
+    @keyframes blink-animation {
+      to {
+        visibility: hidden;
+      }
+    }
+    @-webkit-keyframes blink-animation {
+      to {
+        visibility: hidden;
+      }
+    }
+    </style>
+    <script data-ad-client="ca-pub-1099792537777374" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-199508931-2', 'auto');
+      ga('send', 'pageview');
+    </script>
+  
+  </head>
+  <body data-spy="scroll" data-target=".onpage-navigation" data-offset="80">
+    <main>
+      <div class="page-loader">
+        <div class="loader">Loading...</div>
+      </div>
+      <!--<section class="home-section bg-dark-30" id="home">-->
+
+        <!--<div class="video-player"></div>-->
+        <!--<div class="video-controls-box">-->
+        <!--  <div class="container">-->
+        <!--    <div class="video-controls"><a class="fa fa-volume-up" id="video-volume" href="#">&nbsp;</a>-->
+        <!--    <a class="fa fa-pause mr-5" id="video-play" href="#">&nbsp;</a></div>-->
+        <!--  </div>-->
+        <!--</div>-->
+      <!--<nav class="navbar navbar-custom navbar-fixed-top navbar-transparent" role="navigation">-->
+      <!--  <div class="container">-->
+      <!--    <div class="navbar-header">-->
+      <!--      <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#custom-collapse"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button><a class="navbar-brand" href="index.html">Bkppd</a>-->
+      <!--    </div>-->
+      <!--    <div class="collapse navbar-collapse" id="custom-collapse">-->
+      <!--      <ul class="nav navbar-nav navbar-right">-->
+      <!--        <li class="dropdown"><a  href="https://www.bkppd-balangankab.info" data-toggle="dropdown">Beranda</a></li>      -->
+      <!--      </ul>-->
+      <!--    </div>-->
+      <!--  </div>-->
+      <!--</nav>-->
+      
+      <section class="home-section bg-dark-90 portfolio-page-header parallax-bg" id="home" data-background="assets/images/bg-pahlawan.jpg">
+        <div class="titan-caption">
+          <div class="caption-content">
+            <img src="assets/images/logo.png" alt="logo balangan" width="120">
+            <div class="font-alt mb-5 titan-title-size-3" style="text-shadow:0 7px 5em #000;">Selamat Datang</div>
+            <div class="titan-title-size-2 mb-30" style="text-shadow:0 0 3px #000;">Pelayanan Satu Pintu Badan Kepegawaian Pendidikan dan Pelatihan daerah Kabupaten Balangan</div>
+            
+            <a target="_blank" title="Portal Web" class="section-scroll btn btn-border-w btn-circle btn-lg mb-10" href="https://web.bkppd-balangankab.info/">
+             <span class="icon-newspaper" area-hidden="true"></span> Kunjungi Portal Web
+            </a>
+            <a target="_blank" title="Simpeg" class="section-scroll btn btn-border-w btn-circle btn-lg mb-10" href="http://silka.bkppd-balangankab.info"><span class="icon-profile-male" area-hidden="true"></span> Kunjungi Simpeg</a>
+            <a target="_blank" title="E-Kinerja" class="section-scroll btn btn-border-w btn-circle btn-lg mb-10" href="https://ekinerja.bkppd-balangankab.info"><span class="icon-wallet" area-hidden="true"></span> Kunjungi e-Kinerja</a> 
+            <a target="_blank" title="E-Perilaku" class="section-scroll btn btn-border-w btn-circle btn-lg mb-10" href="https://eprilaku.bkppd-balangankab.info"><span class="icon-clipboard" area-hidden="true"></span> Kunjungi e-Prilaku (360)</a> 
+           
+          </div>
+        </div>
+        
+      </section>
+    <div class="main">
+      <section class="module-extra-small bg-dark">
+          <div class="container">
+            <div class="row">
+              <div class="col-sm-12 col-md-12 col-lg-12 col-lg-offset-2">
+                <div class="callout-text font-alt">
+                  <h4 style="margin-top: 0px; font-;">@timBKPPD</h4>
+                  <p style="margin-bottom: 0px;">&copy; 2019 - <?= date('Y') ?> Badan Kepegawaian Pendidikan dan Pelatihan Daerah</p>
+                </div>
+              </div>
+              <div class="col-sm-6 col-md-4 col-lg-2">
+                <!--<div class="callout-btn-box"><a class="btn btn-w btn-round" href="#">Semua Aplikasi BKPPD</a></div>-->
+              </div>
+            </div>
+          </div>
+        </section>
+        <!--<div class="scroll-up"><a href="#totop"><i class="fa fa-angle-double-up"></i></a></div>-->
+          </div>
+      <!--
+      JavaScripts
+      =============================================
+      -->
+      <script src="assets/lib/jquery/dist/jquery.js"></script>
+      <!--<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" integrity="sha512-bLT0Qm9VnAYZDflyKcBaQ2gg0hSYNQrJ8RilYldYQ1FxQYoCLtUjuuRuZo+fjqhx/qtq/1itJ0C2ejDxltZVFg==" crossorigin="anonymous"></script>-->
+      <script src="assets/lib/bootstrap/dist/js/bootstrap.min.js"></script>
+      <script src="assets/lib/wow/dist/wow.js"></script>
+      <script src="assets/lib/jquery.mb.ytplayer/dist/jquery.mb.YTPlayer.js"></script>
+      <script src="assets/lib/isotope/dist/isotope.pkgd.js"></script>
+      <script src="assets/lib/imagesloaded/imagesloaded.pkgd.js"></script>
+      <script src="assets/lib/flexslider/jquery.flexslider.js"></script>
+      <script src="assets/lib/owl.carousel/dist/owl.carousel.min.js"></script>
+      <script src="assets/lib/smoothscroll.js"></script>
+      <script src="assets/lib/magnific-popup/dist/jquery.magnific-popup.js"></script>
+      <script src="assets/lib/simple-text-rotator/jquery.simple-text-rotator.min.js"></script>
+      <script src="assets/lib/countdown/multi-countdown.js"></script>
+      <script src="assets/js/plugins.js"></script>
+      <script src="assets/js/main.js"></script>
+      <script>
+        // Set the date we're counting down to
+        // var countDownDate = new Date("Nov 5, 2020 00:00:01").getTime();
+        
+        // // Update the count down every 1 second
+        // var x = setInterval(function() {
+        
+        //   // Get today's date and time
+        //   var now = new Date().getTime();
+        
+        //   // Find the distance between now and the count down date
+        //   var distance = countDownDate - now;
+        
+        //   // Time calculations for days, hours, minutes and seconds
+        //   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        //   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        //   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        //   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        
+        //   // Display the result in the element with id="demo"
+        //   document.getElementById("demo").innerHTML = days + "d, " + hours + "h, "
+        //   + minutes + "m, " + seconds + "s ";
+        
+        //   // If the count down is finished, write some text
+        //   if (distance < 0) {
+        //     clearInterval(x);
+        //     document.getElementById("demo").innerHTML = `<a target="_blank" class="section-scroll btn btn-warning btn-lg mb-10" title="Pengumuman kelulusan cpns balangan ta 2019" href="https://drive.google.com/file/d/14dJnrWBTJ2rFXd55v0LTc2zp35JWeQFL/view?usp=sharing"><span class="fa fa-bullhorn" area-hidden="true"></span>(<span style="color:red;" class="blink">New!!!</span>) <h5>Pengumuman kelulusan cpns balangan ta 2019 </h5></a>`;
+        //   }
+        // }, 1000);
+        </script>
+       
+    </body>
+  </html>
