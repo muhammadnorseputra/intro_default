0a4460bfa540c1845ad9c795772b43c3c835c210
muhammadnorseputra <muhammadnorseputra@gmail.com>
2021-11-28 17:37:35 +0700

    up intro

--- index.php
+++ index.php
@@ -24,14 +24,16 @@
     <link rel="stylesheet" href="assets/lib/et-line-font/et-line-font.css">
     <link id="color-scheme" href="assets/css/colors/default.css" rel="stylesheet">
     <!-- <script data-ad-client="ca-pub-1099792537777374" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script> -->
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-206130535-2"></script>
     <script>
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-    ga('create', 'UA-199508931-2', 'auto');
-    ga('send', 'pageview');
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-206130535-2');
     </script>
+
     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6373503432980668"
      crossorigin="anonymous"></script>
   </head>
